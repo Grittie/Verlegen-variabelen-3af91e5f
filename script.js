962403b8b66f1b353b4a624674d5a072ea604ca1
Grittie <grittielars@gmail.com>
2020-02-11 16:27:21 +0100

    Uitwerking van de exercise

---
+++ script.js
@@ -0,0 +1,24 @@
+const pi = 3.14;
+
+function diaMeter() {
+    let diam = document.getElementById('nummer').value;
+
+    let antwoord = diam * pi;
+
+    document.getElementById("antwoord").innerText = antwoord;
+}
+
+function oppervlakte() {
+    let diam = document.getElementById('nummer').value;
+
+    let antwoord2 = diam * diam * pi * 0.25;
+
+    console.log(antwoord2);
+
+    document.getElementById("antwoord2").innerText = antwoord2;
+}
+
+function bereken() {
+    oppervlakte();
+    diaMeter();
+}
